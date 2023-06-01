dc99eca8ef371305f9a89a5eff19c3a4afeb42a5
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2023-06-01 20:52:48 +0200

    feat: solved study schedule algorithm

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    if target_time is None:
+        return None
+    for period in permanence_period:
+        start, end = period
+        if not (isinstance(start, int) and isinstance(end, int)):
+            return None
+        if start <= target_time <= end:
+            counter += 1
+    return counter
