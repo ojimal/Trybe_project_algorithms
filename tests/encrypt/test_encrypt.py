ae122f4ebda2c07402ea0ce14c0ea5dd6200f990
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2023-06-03 10:29:27 +0200

    feat: test encrypt message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    test_cases = [
+        ("message", 4, "ega_ssem"),
+        ("message", 3, "sem_egas"),
+        ("message", 10, "egassem"),
+    ]
+
+    for message, key, expected in test_cases:
+        assert encrypt_message(message, key) == expected
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("hello", "hello")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1234, 4)
